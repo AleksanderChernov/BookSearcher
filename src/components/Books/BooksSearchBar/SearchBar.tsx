f1913d39761c9d5ef77f44822b4dd0b2d02d0047
Aleksander Chernov <aleksanderancher@gmail.com>
2021-12-05 20:15:52 +0300

    fix: semantics, feat: validation model

--- src/components/Books/BooksSearchBar/SearchBar.tsx
+++ src/components/Books/BooksSearchBar/SearchBar.tsx
@@ -1,76 +1,73 @@
 import './SearchBar.css';
 import {
-  Form, Input, Button, Select, Spin, Layout,
+  Form, Input, Button, Select, Spin,
 } from 'antd';
 import Title from 'antd/lib/typography/Title';
 import { SearchOutlined } from '@ant-design/icons';
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import useTypedSelector from '../../../hooks/useTypedSelector';
 import { useBookSearchActions } from '../../../hooks/useBookSearchActions';
+import { searchInputRule } from '../../../validationModels/validationModels';
 
 const { Option } = Select;
 
-enum errEnum {
-  err400 = 'Request failed with status code 400',
-}
-
 const BookSearchBar: React.FC = () => {
-  const [bookName, setBookName] = useState('');
-  const [chosenSubject, setChosenSubject] = useState('');
-  const [chosenRelevance, setChosenRelevance] = useState('relevance');
   const { searchForBooks } = useBookSearchActions();
   const { error, isLoading } = useTypedSelector(
     (state) => state.bookItems,
   );
 
-  console.log(chosenSubject);
-  console.log(bookName);
-  console.log(chosenRelevance);
-
-  const onFinish = (e: React.FormEvent<HTMLFormElement>) => {
-    /* e.preventDefault(); */
-    searchForBooks(bookName, chosenSubject, chosenRelevance);
+  const onFinish = (values: string) => {
+    const entries = Object.values(values);
+    searchForBooks(entries[0], entries[1], entries[2]);
   };
 
-  const onInputChangeBook = useCallback((e) => setBookName(e.target.value), []);
-  const changeSubject = useCallback((value) => setChosenSubject(value), []);
-  const changeRelevance = useCallback((value) => setChosenRelevance(value), []);
+  const bookSubjectOptions = [
+    { value: '', title: 'Любой' },
+    { value: 'art', title: 'Искусство' },
+    { value: 'biography', title: 'Биография' },
+    { value: 'computers', title: 'Компьютеры' },
+    { value: 'history', title: 'История' },
+    { value: 'medical', title: 'Медицина' },
+    { value: 'poetry', title: 'Поэзия' },
+  ];
 
   return (
     <div className="search-bar__wrapper">
       <Title className="search-bar__title" level={2}>Поиск</Title>
-      <Form onFinish={onFinish} className="search-bar__form">
-        <Form.Item name="Название" rules={[{ required: true, message: 'Поле необходимо заполнить' }, { max: 35, message: 'Максимум 35 символов' }]}>
+      <Form onFinish={onFinish} name="form" className="search-bar__form">
+        <Form.Item name="bookInput" rules={searchInputRule.validation}>
           <Input
             placeholder="Название"
             className="search-bar__input"
-            value={bookName}
-            onChange={(e) => onInputChangeBook(e)}
           />
         </Form.Item>
         <Title className="search-bar__title" level={5}>Жанры</Title>
-        <Select defaultValue="Любой" onChange={changeSubject}>
-          <Option value="all">Любой</Option>
-          <Option value="art">Искусство</Option>
-          <Option value="biography">Биография</Option>
-          <Option value="computers">Компьютеры</Option>
-          <Option value="history">История</Option>
-          <Option value="medical">Медицина</Option>
-          <Option value="poetry">Поэзия</Option>
-        </Select>
+        <Form.Item name="bookSubject" initialValue="">
+          <Select defaultValue="Любой">
+            {bookSubjectOptions.map(
+              (option) => (
+                <Option value={option.value}>
+                  {option.title}
+                </Option>
+              ),
+            )}
+          </Select>
+        </Form.Item>
         <Title className="search-bar__title" level={5}>Сортировка</Title>
-        <Select defaultValue="relevance" onChange={changeRelevance}>
-          <Option value="relevance">Самое релевантное</Option>
-          <Option value="newest">Самое новое</Option>
-        </Select>
+        <Form.Item name="bookRelevance" initialValue="relevance">
+          <Select defaultValue="relevance">
+            <Option value="relevance">Самое релевантное</Option>
+            <Option value="newest">Самое новое</Option>
+          </Select>
+        </Form.Item>
         <Button type="primary" htmlType="submit" className="search-bar__button" icon={<SearchOutlined />} size="large">
           Искать
         </Button>
       </Form>
-      {error !== errEnum.err400 && (
+      {error && (
         <h3 className="search-bar__error">{error}</h3>
       )}
-      {error === errEnum.err400 ? 'Введите парaметры запроса' : ''}
       {isLoading && (
         <>
           <h3 className="search-bar__loading">Вывожу результаты</h3>
---
+++ src/validationModels/validationModels.ts
@@ -0,0 +1,3 @@
+export const searchInputRule = {
+  validation: [{ required: true, message: 'Поле необходимо заполнить' }, { max: 35, message: 'Максимум 35 символов' }],
+};
